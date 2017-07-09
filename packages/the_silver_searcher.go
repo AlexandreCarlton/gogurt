d56209468e77312076177ecd3ce32c8de34d8c80
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2017-07-09 17:20:58 +1000

    Add the_silver_searcher package
    This didn't work super with pkg-config, so I had to directly modify the
    flags used.

---
+++ packages/the_silver_searcher.go
@@ -0,0 +1,82 @@
+package packages
+
+import (
+	"fmt"
+	"github.com/alexandrecarlton/gogurt"
+)
+
+type TheSilverSearcher struct{}
+
+func (ag TheSilverSearcher) Name() string {
+	return "the_silver_searcher"
+}
+
+func (ag TheSilverSearcher) URL(version string) string {
+	return fmt.Sprintf("http://geoff.greer.fm/ag/releases/the_silver_searcher-%s.tar.gz", version)
+}
+
+func (ag TheSilverSearcher) Build(config gogurt.Config) error {
+	// pkg-config is messing up for some reason.
+	gogurt.ReplaceInFile("configure", "^ *PKG_CHECK_MODULES.*", "")
+	gogurt.ReplaceInFile("configure.ac", "^ *PKG_CHECK_MODULES.*", "")
+
+	configure := gogurt.ConfigureCmd{
+		Prefix: config.InstallDir(ag),
+		Args: []string{
+			"--enable-zlib",
+			"--enable-lzma",
+		},
+		CFlags: []string{
+			"-I" + config.IncludeDir(Pcre{}),
+			"-I" + config.IncludeDir(XZ{}),
+			"-I" + config.IncludeDir(Zlib{}),
+		},
+		CppFlags: []string{
+			"-I" + config.IncludeDir(Pcre{}),
+			"-I" + config.IncludeDir(XZ{}),
+			"-I" + config.IncludeDir(Zlib{}),
+		},
+		LdFlags: []string{
+			"-L" + config.LibDir(Pcre{}),
+			"-L" + config.LibDir(XZ{}),
+			"-L" + config.LibDir(Zlib{}),
+		},
+		Libs: []string{
+			"-lpcre",
+			"-llzma",
+			"-lz",
+		},
+	}.Cmd()
+
+	configure.Env = append(
+		configure.Env,
+		"PCRE_CFLAGS=-I" + config.IncludeDir(Pcre{}),
+		"PCRE_LIBS=-L" + config.LibDir(Pcre{}) + " -lpcre",
+		"LZMA_CFLAGS=-I" + config.IncludeDir(XZ{}),
+		"LZMA_LIBS=-L" + config.LibDir(XZ{}) + " -llzma",
+	)
+	if err := configure.Run(); err != nil {
+		return err
+	}
+	make := gogurt.MakeCmd{
+		Jobs: config.NumCores,
+		Paths: []string{
+			config.BinDir(AutoMake{}),
+		},
+	}.Cmd()
+	return make.Run()
+}
+
+func (ag TheSilverSearcher) Install(config gogurt.Config) error {
+	makeInstall := gogurt.MakeCmd{Args: []string{"install"}}.Cmd()
+	return makeInstall.Run()
+}
+
+func (ag TheSilverSearcher) Dependencies() []gogurt.Package {
+	return []gogurt.Package{
+		AutoMake{},
+		Pcre{},
+		XZ{},
+		Zlib{},
+	}
+}
