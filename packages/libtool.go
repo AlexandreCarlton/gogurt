37f6d17f5bd0c28a602f79b12bc4bf837f56e08e
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2017-07-09 15:03:07 +1000

    Add libtool package
    This might help us in installing pkg-config (which will help us in
    installing ag). Geez, talk about yak shaving.

---
+++ packages/libtool.go
@@ -0,0 +1,49 @@
+package packages
+
+import (
+	"fmt"
+	"github.com/alexandrecarlton/gogurt"
+)
+
+type LibTool struct{}
+
+func (libtool LibTool) Name() string {
+	return "libtool"
+}
+
+func (libtool LibTool) URL(version string) string {
+	return fmt.Sprintf("https://ftp.gnu.org/gnu/libtool/libtool-%s.tar.gz", version)
+}
+
+func (libtool LibTool) Build(config gogurt.Config) error {
+	configure := gogurt.ConfigureCmd{
+		Prefix: config.InstallDir(libtool),
+		Args: []string{
+			"--disable-shared",
+			"--enable-static",
+			"--enable-ltdl-install",
+		},
+	}.Cmd()
+	if err := configure.Run(); err != nil {
+		return err
+	}
+	make := gogurt.MakeCmd{
+		Jobs: config.NumCores,
+		Paths: []string{
+			config.BinDir(AutoMake{}),
+		},
+	}.Cmd()
+	return make.Run()
+}
+
+func (libtool LibTool) Install(config gogurt.Config) error {
+	makeInstall := gogurt.MakeCmd{Args: []string{"install"}}.Cmd()
+	return makeInstall.Run()
+}
+
+func (libtool LibTool) Dependencies() []gogurt.Package {
+	return []gogurt.Package{
+		AutoMake{},
+		// TODO: Requires help2man
+	}
+}
