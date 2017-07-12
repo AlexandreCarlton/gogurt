a63d3533ef74c9c61f0453c603db2a46e587af41
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2017-07-12 21:12:42 +1000

    Add MPFR package
    This is another library used by gcc, for arbitrary-precision floating
    point computation.

---
+++ packages/mpfr.go
@@ -0,0 +1,50 @@
+package packages
+
+import (
+	"fmt"
+	"github.com/alexandrecarlton/gogurt"
+)
+
+type MPFR struct{}
+
+func (mpfr MPFR) Name() string {
+	return "mpfr"
+}
+
+func (mpfr MPFR) URL(version string) string {
+	return fmt.Sprintf("http://www.mpfr.org/mpfr-%s/mpfr-%s.tar.gz", version, version)
+}
+
+func (mpfr MPFR) Build(config gogurt.Config) error {
+	configure := gogurt.ConfigureCmd{
+		Prefix: config.InstallDir(mpfr),
+		Args: []string{
+			"--disable-shared",
+			"--enable-static",
+			"--with-gmp=" + config.InstallDir(GMP{}),
+			"--enable-thread-safe",
+		},
+	}.Cmd()
+	if err := configure.Run(); err != nil {
+		return err
+	}
+	make := gogurt.MakeCmd{
+		Jobs: config.NumCores,
+		Paths: []string{
+			config.BinDir(AutoMake{}),
+		},
+	}.Cmd()
+	return make.Run()
+}
+
+func (mpfr MPFR) Install(config gogurt.Config) error {
+	makeInstall := gogurt.MakeCmd{Args: []string{"install"}}.Cmd()
+	return makeInstall.Run()
+}
+
+func (mpfr MPFR) Dependencies() []gogurt.Package {
+	return []gogurt.Package{
+		AutoMake{},
+		GMP{},
+	}
+}
