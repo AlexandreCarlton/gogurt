6c347eb6e8686d0f377ce477e368fc18244bdf6a
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2017-07-22 20:58:18 +1000

    Add package gdb
    Haven't been fully able to test this since even the system-provided gdb
    is broken inside docker. It behaves identically to it, though.

---
+++ packages/gdb.go
@@ -0,0 +1,103 @@
+package packages
+
+import (
+	"fmt"
+	"path/filepath"
+	"github.com/alexandrecarlton/gogurt"
+)
+
+type GDB struct{}
+
+func (gdb GDB) Name() string {
+	return "gdb"
+}
+
+func (gdb GDB) URL(version string) string {
+	return fmt.Sprintf("https://ftp.gnu.org/gnu/gdb/gdb-%s.tar.gz", version)
+}
+
+func (gdb GDB) Build(config gogurt.Config) error {
+	// We only want to build what's in gdb/
+	gdbDir := filepath.Join(config.BuildDir(gdb), "gdb")
+
+	// Python was statically linked with OpenSSL and Zlib.
+	// This means that the python-config.py should link this in.
+	// However, 'python' should come BEFORE the libraries it links to.
+	// TODO: Raise PR.
+	gogurt.ReplaceInFile(
+		filepath.Join(gdbDir, "python", "python-config.py"),
+		"libs = \\[\\]",
+		"libs = ['-lpython' + pyver]")
+	gogurt.ReplaceInFile(
+		filepath.Join(gdbDir, "python", "python-config.py"),
+		".*libs\\.append\\('-lpython'.*",
+		"")
+	// Ncurses also generates a tinfo library as well, which we must link in.
+	gogurt.ReplaceInFile(
+		filepath.Join(gdbDir, "configure"),
+		"'' ncurses",
+		"'ncursesw -ltinfow' ncurses")
+
+	configure := gogurt.ConfigureCmd{
+		Prefix: config.InstallDir(gdb),
+		Args: []string{
+			"--disable-shared",
+			"--enable-static",
+			"--enable-tui",
+			"--with-curses",
+			"--with-python=" + config.InstallDir(Python2{}),
+			"--with-lzma",
+			"--with-liblzma-prefix=" + config.InstallDir(XZ{}),
+			"--with-libexpat",
+			"--with-libexpat-prefix=" + config.InstallDir(Expat{}),
+			//"--with-system-zlib",
+			//"--with-system-readline",
+		},
+		CFlags: []string{
+			"-I" + config.IncludeDir(Ncurses{}),
+			"-I" + config.IncludeDir(Zlib{}),
+			// These are so that we can statically link Python.
+			"-I" + config.IncludeDir(OpenSSL{}),
+		},
+		CxxFlags: []string{
+			"-I" + config.IncludeDir(Ncurses{}),
+			"-I" + config.IncludeDir(Zlib{}),
+			// These are so that we can statically link Python.
+			"-I" + config.IncludeDir(OpenSSL{}),
+		},
+		LdFlags: []string{
+			"-L" + config.LibDir(Ncurses{}),
+			"-L" + config.LibDir(Zlib{}),
+			// For Python.
+			"-L" + config.LibDir(OpenSSL{}),
+		},
+		Libs: []string{
+			"-ltinfow",
+		},
+	}.Cmd()
+	if err := configure.Run(); err != nil {
+		return err
+	}
+	make := gogurt.MakeCmd{
+		Jobs: config.NumCores,
+	}.Cmd()
+	return make.Run()
+}
+
+func (gdb GDB) Install(config gogurt.Config) error {
+	makeInstall := gogurt.MakeCmd{
+		Args: []string{
+			"install",
+		},
+	}.Cmd()
+	return makeInstall.Run()
+}
+
+func (gdb GDB) Dependencies() []gogurt.Package {
+	return []gogurt.Package{
+		Expat{},
+		Ncurses{},
+		XZ{},
+		Python2{},
+	}
+}
