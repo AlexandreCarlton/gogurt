d142666bcf3554070113ffe099829262e08300d1
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2017-09-03 00:12:10 +1000

    Add package global
    The errors initially experienced with this were due to an error in
    extracting tar archives (not setting their mtime/atime correctly,
    resulting in some files being 'newer' than others, causing automake to
    kick in).

---
+++ packages/global.go
@@ -0,0 +1,58 @@
+package packages
+
+import (
+	"fmt"
+	"github.com/alexandrecarlton/gogurt"
+)
+
+type Global struct{}
+
+func (global Global) Name() string {
+	return "global"
+}
+
+func (global Global) URL(version string) string {
+	return fmt.Sprintf("https://ftp.gnu.org/gnu/global/global-%s.tar.gz", version)
+}
+
+func (global Global) Build(config gogurt.Config) error {
+	configure := gogurt.ConfigureCmd{
+		Prefix: config.InstallDir(global),
+		Args: []string{
+			"--enable-static",
+			"--disable-shared",
+			"--with-universal-ctags=" + config.BinDir(UniversalCTags{}),
+			"--with-ncurses=" + config.InstallDir(Ncurses{}),
+			"--disable-gtagscscope", // for now.
+			"--with-included-ltdl", // could use our own libtool?
+		},
+		Paths: []string{
+			config.BinDir(AutoMake{}),
+			config.BinDir(AutoConf{}),
+		},
+	}.Cmd()
+	if err := configure.Run(); err != nil {
+		return err
+	}
+	make := gogurt.MakeCmd{
+		Jobs: config.NumCores,
+		Paths: []string{
+			config.BinDir(AutoMake{}),
+			config.BinDir(AutoConf{}),
+		},
+	}.Cmd()
+	return make.Run()
+}
+
+func (global Global) Install(config gogurt.Config) error {
+	makeInstall := gogurt.MakeCmd{Args: []string{"install"}}.Cmd()
+	return makeInstall.Run()
+}
+
+func (global Global) Dependencies() []gogurt.Package {
+	return []gogurt.Package{
+		AutoMake{},
+		Ncurses{},
+		UniversalCTags{},
+	}
+}
