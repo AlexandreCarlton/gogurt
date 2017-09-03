35d613cda5e449bd9a80ff8badb1f5901ca2cb0b
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2017-09-03 20:36:11 +1000

    Fix automake hacks to get it working
    Turns out this was due to the error I made in extracting tar archives.

--- packages/automake.go
+++ packages/automake.go
@@ -2,7 +2,6 @@ package packages
 
 import (
 	"fmt"
-	"os/exec"
 	"github.com/alexandrecarlton/gogurt"
 )
 
@@ -16,30 +15,27 @@ func (automake AutoMake) URL(version string) string {
 	return fmt.Sprintf("https://ftp.gnu.org/gnu/automake/automake-%s.tar.gz", version)
 }
 
-// We hit a slight problem here.
-// For some reason, a configure followed by a make would produce:
-// Can't locate <gogurt_build>/automake/1.15.1/bin/automake in @INC (@INC contains: /usr/local/lib64/perl5 /usr/local/share/perl5 /usr/lib64/perl5/vendor_perl /usr/share/perl5/vendor_perl /usr/lib64/perl5 /usr/share/perl5 .) at <gogurt_build>/automake/1.15.1/t/wrap/automake-1.15 line 27.
-// For this reason, we run bootstrap to regenerate the configure script.
-// However, this would require that the documentation is regenerated using texinfo (which requires a new version of automake). For this reason, we only build the executable scripts.
 func (automake AutoMake) Build(config gogurt.Config) error {
-	bootstrap := exec.Command("./bootstrap")
-	if err := bootstrap.Run(); err != nil {
-		return err
-	}
 	configure := gogurt.ConfigureCmd{
 		Prefix: config.InstallDir(automake),
 	}.Cmd()
-	return configure.Run()
+	if err := configure.Run(); err != nil {
+		return err
+	}
+	make := gogurt.MakeCmd{
+		Jobs: config.NumCores,
+	}.Cmd()
+	return make.Run()
 }
 
 func (automake AutoMake) Install(config gogurt.Config) error {
-	makeInstall := gogurt.MakeCmd{Args: []string{"install-exec-am", "prefix=" + config.InstallDir(automake)}}.Cmd()
+	makeInstall := gogurt.MakeCmd{
+		Args: []string{"install"},
+		Jobs: config.NumCores,
+	}.Cmd()
 	return makeInstall.Run()
 }
 
 func (automake AutoMake) Dependencies() []gogurt.Package {
-	return []gogurt.Package{
-		AutoConf{},
-	}
+	return []gogurt.Package{}
 }
-
